b929464f42afcff4bac237e8f2b5f17be83059f9
DariaMajc <daria.majchrowska@wp.pl>
2022-04-02 14:54:05 +0200

    hakaton2

---
+++ hangman.md
@@ -0,0 +1,16 @@
+<center><h1>HANGMAN GAME</h1>
+
+
+The user has 6 trials to guess the word.
+<br>
+User gives the program letters or a complete word.
+
+________________________________________________________
+
+
+<strong>solution concept</strong>
+<br>
+Computer open a file with words and draw one for user.
+Next in the while loop, if statements are executed to check if the letter or the whole word matches the selected word drawn by computer.
+Terminal print a hangman part as always as user lose 1 turn.
+</center>
--- hangman.py
+++ hangman.py
@@ -1,14 +1,14 @@
 import random
+
 with open('txt.txt', 'r') as fopen:
     lines = fopen.readlines()
 
 word_guess = random.choice(lines).strip()
 counter = len(word_guess)
-gamer = input('Jak masz na imię drogi graczu? -> ')
-print('Witam w grze wisielec', gamer)
-print('Haslo sklada sie z ', counter, 'liter.')
+print('Hasło to jeden ze składników pancakes')
+print('Hasło sklada sie z ', counter, 'liter.')
 turns = 6
-print('Masz', 6, 'prob, aby odgadnac haslo')
+print('Masz', 6, 'prob, aby odgadnac hasło!')
 
 def game(user_guess):
     turns=6
@@ -31,7 +31,7 @@ def game(user_guess):
             break
 
         if guess in word_guess:
-            turns -=1
+            turns -= 1
             for index in range(counter):
                 if word_guess[index] == guess:
                     user_guess[index] = guess
@@ -50,7 +50,7 @@ def game(user_guess):
 
 
         if turns == 0:
-            print("_________")
+            print(" _________")
             print("|	 |")
             print("|	 O")
             print("|	\|/")
@@ -105,77 +105,9 @@ def game(user_guess):
 
 
         print('Zostało Ci prób ->', turns)
-        print('Podaj kolejna litere')
+        print('Podaj kolejna litere.')
 
 
 
 turns = 6
 game(turns)
-#
-# def print_scaffold(guesses, wd): # prints the scaffold
-# 		if (guesses == 0):
-# 				print "_________"
-# 				print "|	 |"
-# 				print "|"
-# 				print "|"
-# 				print "|"
-# 				print "|"
-# 				print "|________"
-# 		elif (guesses == 1):
-# 				print "_________"
-# 				print "|	 |"
-# 				print "|	 O"
-# 				print "|"
-# 				print "|"
-# 				print "|"
-# 				print "|________"
-# 		elif (guesses == 2):
-# 				print "_________"
-# 				print "|	 |"
-# 				print "|	 O"
-# 				print "|	 |"
-# 				print "|	 |"
-# 				print "|"
-# 				print "|________"
-# 		elif (guesses == 3):
-# 				print "_________"
-# 				print "|	 |"
-# 				print "|	 O"
-# 				print "|	\|"
-# 				print "|	 |"
-# 				print "|"
-# 				print "|________"
-# 		elif (guesses == 4):
-# 				print "_________"
-# 				print "|	 |"
-# 				print "|	 O"
-# 				print "|	\|/"
-# 				print "|	 |"
-# 				print "|"
-# 				print "|________"
-# 		elif (guesses == 5):
-# 				print "_________"
-# 				print "|	 |"
-# 				print "|	 O"
-# 				print "|	\|/"
-# 				print "|	 |"
-# 				print "|	/ "
-# 				print "|________"
-# 		elif (guesses == 6):
-# 				print "_________"
-# 				print "|	 |"
-# 				print "|	 O"
-# 				print "|	\|/"
-# 				print "|	 |"
-# 				print "|	/ \ "
-# 				print "|________"
-# 				print "\n"
-# 				print "The word was %s." %wd
-# 				print "\n"
-# 				print "\nYOU LOSE! TRY AGAIN!"
-# 				print "\nWould you like to play again, type 1 for yes or 2 for no?"
-# 				again = str(raw_input("> "))
-# 				again = again.lower()
-# 				if again == "1":
-# 				  hangMan()
-# 				return
